34f1c285de9e6289a3554c14271eb5935c2346e8
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-08-14 14:24:50 -0700

    Loop

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-    for (int i = 5; i > 0; --i) {
+   /*    for (int i = 5; i > 0; --i) {
+        }
+     System.out.println("Hello world!" + i);
+        int i = 0;
+        while (i > 0) {
+            System.out.println("Hello world!" + i);
+            i--;
+        } */
+    // To quit something in while loop just use this
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello world!" + i);
-    }
+        String input = "";
+                while (!input.equals("quit")) {
+                    System.out.println("Input: ");
+                    input = scanner.next().toLowerCase();
+                    System.out.println(input);
+                }
     }
 }
