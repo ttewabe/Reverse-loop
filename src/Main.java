5049886a4e5e4f91a180b15a8005e92ebd09756f
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-08-14 15:41:43 -0700

    Break and Continue

--- src/Main.java
+++ src/Main.java
@@ -17,12 +17,11 @@ public class Main {
                 while (!input.equals("quit")) {
                     System.out.println("Input: ");
                     input = scanner.next().toLowerCase();
+                    if (input.equals("pass"))
+                        continue;
+                        if (input.equals("quit"))
+                            break;
                     System.out.println(input);
                 }
-        do {
-            System.out.println("Input: ");
-            input = scanner.next().toLowerCase();
-            System.out.println(input);
-        } while (!input.equals("quit"));
     }
 }
